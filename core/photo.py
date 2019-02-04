58f6e3c8fd02b2670634a27ad4807f09cbb07515
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-02-05 01:37:14 +0530

    Photo.py ( Main executable file )

---
+++ core/photo.py
@@ -0,0 +1,94 @@
+import os
+import cv2
+import numpy as np
+import time
+import logo
+import platform
+import colors
+
+# Some global declaration shits......!!!
+if(platform.system == 'Windows'):
+    path_symbol = "\\"
+else:
+    path_symbol = "/"
+
+def template_images(temp_image):
+
+    try:
+        os.mkdir('Template images')
+        colors.success("Folder Created")
+
+    except:
+        colors.error("Folder already exsist.")
+        pass
+
+    os.chdir(os.getcwd() + "{}Template images".format(path_symbol))
+
+    colors.success("Directory set to new location ")
+
+    # Loading Template image.
+    colors.success("Image read into memory")
+
+    x = y = 0
+    width = [160, 320, 480, 640]
+    height = [160, 320, 480]
+    count = 0
+
+    for h in height:
+        for w in width:
+            image_section = temp_image[y:h, x:w]
+            x = w
+            cv2.imwrite(str(count) + ".jpg", image_section)
+            count += 1
+
+        x = 0
+        y = h
+
+def check_match():
+
+    list_temp_images=os.listdir(os.getcwd()+"{}Template images".format(path_symbol))
+    colors.success("template image list grabbed. ")
+    list_search_images=os.listdir(os.getcwd()+"{}images".format(path_symbol))
+    colors.success("search image list grabbed ")
+
+    print("\n{}----------------------------------------------------------------------{}".format(colors.red,colors.green))
+    print("\n\t {}:: Similar images found are :: \n".format(colors.lightgreen))
+    for path in list_search_images:
+
+        checked = []
+        pos = 0
+        src_image = cv2.imread('images{}'.format(path_symbol) + path, 1)
+        src_gray = cv2.cvtColor(src_image, cv2.COLOR_BGR2GRAY)
+
+        while(pos<12):
+
+            template_path=list_temp_images[pos]
+            template_image=cv2.imread('Template images{}'.format(path_symbol)+template_path, 0)
+            result = cv2.matchTemplate(src_gray, template_image, cv2.TM_CCOEFF_NORMED)
+
+            thresh = 0.9
+            loc = np.where(result > thresh)
+            if str(loc[0]) == str(loc[1]):
+                checked.append("False")
+                break
+            else:
+                checked.append("True")
+            pos+= 1
+        if "False" not in checked:
+            print("Image : {}".format(path))
+
+
+if __name__ == '__main__':
+
+    logo.banner()
+    print("\n")
+    template_path = str(input("[ {}!{} ] Enter Template path : {}".format(colors.white,colors.end,colors.lightgreen)))
+    print("\n")     # Some serious end of line for UI purpose LOL ...
+    template = cv2.imread(template_path, 1)
+    colors.process("Creating section of template image.")
+    template_images(template)
+    colors.success("12 Section of template image created.")
+    os.chdir("..{}".format(path_symbol))
+    colors.process("Setting 'Image matching' as current directory.")
+    check_match()
+    print("{}\nThankyou for using my tool".format(colors.blue))
