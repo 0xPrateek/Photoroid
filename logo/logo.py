1d0488282548c790127c39b4b6b1937ea4461612
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-02-06 08:33:32 -0800

    adding Logo.py in Logo folder

---
+++ logo/logo.py
@@ -0,0 +1,14 @@
+import colors as color
+def banner():
+
+    print(r'''
+    {}
+             /▔▔▔▔▔\/▔▔/  /▔▔/▔▔▔▔▔\/▔▔▔▔▔▔▔▔▔▔/▔▔▔▔▔▔\ /▔▔▔▔▔▔▔\/▔▔▔▔▔\/▔▔/▔▔▔▔▔▔\
+            / {}/▔▔/{} /  /  /  / {}/▔▔/{} /▔▔▔▔/ /▔▔▔/  {}/▔▔/{}  /  {}/▔▔/{}  / {}/▔▔/{} /  / {}/▔ \{}  /
+           /  {}▔▔▔{} /   ▔▔   / {}/  /{} /    / /   /  {}/  /{}  /   {}▔▔{}   / {}/  /{} /  / {}/   /{} /
+          / /▔▔▔▔/  /▔▔/  / {}/  /{} /    / /   /  {}/  /{}  /  /\ \▔▔/ {}/  /{} /  / {}/   /{} /
+         / /    /  /  /  /\ {}▔▔{}  /    / /    \  {}▔▔{}   /  /  \ \ \{} ▔▔{}  /  /  {}▔▔▔▔{} /
+    {}     ▔▔     ▔▔▔   ▔▔▔  ▔▔▔▔▔     ▔▔      ▔▔▔▔▔▔ ▔▔▔    ▔▔  ▔▔▔▔ ▔▔▔ ▔▔▔▔▔▔▔
+                                                       {} Made by :- pr0t0n
+                                                       {} P.S. Project is under Beta mode.
+    '''.format(color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.lightblue,color.red,color.red,color.lightgreen,color.blue))
