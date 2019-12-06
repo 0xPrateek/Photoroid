1b9e59fb5e77cd6ce5ef546f7a1043463b01ead9
VishalS99 <s-vishal1999@hotmail.com>
2019-12-06 16:39:22 +0530

    feat: updated formatting for long lines

--- core/logo.py
+++ core/logo.py
@@ -13,4 +13,14 @@ def banner():
     {}     ▔▔     ▔▔▔   ▔▔▔  ▔▔▔▔▔     ▔▔      ▔▔▔▔▔▔ ▔▔▔    ▔▔  ▔▔▔▔ ▔▔▔ ▔▔▔▔▔▔▔
                                                        {} Made by :- pr0t0n
                                                        {} P.S. Project is under Beta mode.
-    '''.format(color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.lightblue, color.red, color.red, color.lightgreen, color.blue))
+    '''
+          .format(color.red, color.lightblue, color.red, color.lightblue, color.red,
+                  color.lightblue, color.red, color.lightblue, color.red, color.lightblue,
+                  color.red, color.lightblue, color.red, color.lightblue, color.red,
+                  color.lightblue, color.red, color.lightblue, color.red, color.lightblue,
+                  color.red, color.lightblue, color.red, color.lightblue, color.red,
+                  color.lightblue, color.red, color.lightblue, color.red, color.lightblue,
+                  color.red, color.lightblue, color.red, color.lightblue, color.red,
+                  color.lightblue, color.red, color.lightblue, color.red, color.lightblue,
+                  color.red, color.red, color.lightgreen, color.blue)
+          )
